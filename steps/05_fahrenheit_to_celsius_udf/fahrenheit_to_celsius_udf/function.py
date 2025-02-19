e35666d8eb66db0cd27f202f577d4fe9b71ab256
sahilk710 <kasliwal.s@northeastern.edu>
2025-02-19 18:48:34 -0500

    Update UDF to use scipy for temperature conversion

--- steps/05_fahrenheit_to_celsius_udf/fahrenheit_to_celsius_udf/function.py
+++ steps/05_fahrenheit_to_celsius_udf/fahrenheit_to_celsius_udf/function.py
@@ -17,6 +17,7 @@ def main(temp_f: float) -> float:
     return convert_temperature(float(temp_f), 'F', 'C')
 
 
+# For local debugging
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         print(main(*sys.argv[1:]))  # type: ignore
